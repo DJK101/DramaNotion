e95100beca9b033172a92196435e2a2ee7bbe1b7
DJK101 <danieljeffersonkennedy@gmail.com>
2024-11-29 11:31:44 +0000

    Update naming conventions

--- receipt_names.py
+++ receipt_names.py
@@ -24,15 +24,15 @@ def get_pages():
     return data["results"]
 
 
-def getName(result) -> str:
+def get_name(result) -> str:
     return result["properties"]["Name"]["title"][0]["plain_text"]
 
 
-def getFile(result):
+def get_file(result):
     return result["properties"]["Receipt File"]["files"][0]
 
 
-def getPurchaseDate(result):
+def get_purchase_date(result):
     return result["properties"]["Receipt Date"]["date"]["start"]
 
 
@@ -53,12 +53,13 @@ for result in get_pages():
         continue
 
     page_id = result["id"]
-    oldName = getFile(result)["name"]
-    newName = getPurchaseDate(result) + "_" + getName(result).replace(" ", "") + ".pdf"
-    if oldName != newName:
-        print("Old Name: " + oldName + " | New Name: " + newName)
+
+    old_file_name = get_file(result)["name"]
+    new_file_name = get_purchase_date(result) + "_" + get_name(result).replace(" ", "") + ".pdf"
+    if old_file_name != new_file_name:
+        print("Old Name: " + old_file_name + " | New Name: " + new_file_name)
         properties = {"Receipt File": result["properties"]["Receipt File"]}
-        properties["Receipt File"]["files"][0]["name"] = newName
+        properties["Receipt File"]["files"][0]["name"] = new_file_name
         update_properties(page_id, properties)
     else:
-        print("Name unchanged: " + oldName)
+        print("Name unchanged: " + old_file_name)
